b7b46a882c2335863533aaae262c9c579f67b7b5
Alex Vinogradov <avinogradov.lvov@gmail.com>
2017-04-10 14:55:57 +0000

    1004

--- js/index.js
+++ js/index.js
@@ -1,11 +1,33 @@
-function initMap() {
-        var uluru = {lat: -25.363, lng: 131.044};
-        var map = new global.google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
-          center: uluru
-        });
-        var marker = new global.google.maps.Marker({
-          position: uluru,
-          map: map
-        });
-      }
+var $contactForm = $('#contact-form');
+$contactForm.submit(function(e) {
+	e.preventDefault();
+	$.ajax({
+		url: '//formspree.io/avinogradov.lvov@gmail.com',
+		method: 'POST',
+		data: $(this).serialize(),
+		dataType: 'json',
+		beforeSend: function() {
+			$contactForm.append('<div class="alert alert--loading">Sending message…</div>');
+		},
+		success: function(data) {
+			$contactForm.find('.alert--loading').hide();
+			$contactForm.append('<div class="alert alert--success">Message sent!</div>');
+		},
+		error: function(err) {
+			$contactForm.find('.alert--loading').hide();
+			$contactForm.append('<div class="alert alert--error">Ops, there was an error.</div>');
+		}
+	});
+});
+
+
+
+
+
+
+
+
+
+
+
+      
